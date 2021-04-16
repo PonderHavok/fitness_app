380c6357f286996d6928d190ec2a2448571969df
Jonny Talbott <transverseponderer@gmail.com>
2021-04-15 23:39:54 -0400

    finalizing format corrections.

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(logger("dev"))
+app.use(logger("dev"));
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 app.use(require("./routes/api.js"));
-app.use(require("./routes/view"))
+app.use(require("./routes/view"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
